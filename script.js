622e551f5a8844aca6feaf41bed7b50da02f078c
Dmitriy <letos.d.r@mail.ru>
2020-08-12 18:55:45 +0700

    answers for questions

--- script.js
+++ script.js
@@ -6,3 +6,7 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
+const movie1 = prompt("один из последних просмотренных фильмов?", "");
+const ocen1 = prompt("На сколько бы вы его оценили?", "");
+const movie2 = prompt("один из последних просмотренных фильмов?", "");
+const ocen2 = prompt("На сколько бы вы его оценили?", "");
