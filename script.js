9c563ade745bc5fa190bcd13e05f1adde9931dfa
Dmitriy <letos.d.r@mail.ru>
2020-08-12 19:00:40 +0700

    console log

--- script.js
+++ script.js
@@ -10,3 +10,8 @@ const movie1 = prompt("один из последних просмотренны
 const ocen1 = prompt("На сколько бы вы его оценили?", "");
 const movie2 = prompt("один из последних просмотренных фильмов?", "");
 const ocen2 = prompt("На сколько бы вы его оценили?", "");
+
+personalMovieDB.movies[movie1] = ocen1;
+personalMovieDB.movies[movie2] = ocen2;
+
+console.log(personalMovieDB);
