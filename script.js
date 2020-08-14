061d6f70c21b9f5cfcbf06f7f4e0e3ca863a9afb
Dmitriy <letos.d.r@mail.ru>
2020-08-14 18:14:39 +0700

    vtoroe zadanie

--- script.js
+++ script.js
@@ -13,6 +13,26 @@ const personalMovieDB = {
 for(let i = 1; i <= 2; i++) {
     let a = prompt("один из последних просмотренных фильмов?", "");
     let b = prompt("На сколько бы вы его оценили?", "");
+        if(a === null){
+            i = i - 1;
+            continue;
+        } else if(b === null){
+            i = i - 1;
+            continue;
+        } else if(a === ""){
+            i = i - 1;
+            continue;
+        } else if(b === ""){
+            i = i - 1;
+            continue;
+        } else if(a.length >= 50){
+            i = i - 1;
+            continue;
+        } else if(b.length >= 50){
+            i = i - 1;
+            continue;
+        }
+            
     personalMovieDB.movies[a] = b;
 }
 // personalMovieDB.movies[movie1] = ocen1;
