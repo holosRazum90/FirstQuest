70df57cdc7e209d6666ccc3a36d5a940ae85ffee
Dmitriy <letos.d.r@mail.ru>
2020-08-15 17:41:01 +0700

    second way

--- script.js
+++ script.js
@@ -13,33 +13,42 @@ const personalMovieDB = {
 for(let i = 1; i <= 2; i++) {
     let a = prompt("один из последних просмотренных фильмов?", "");
     let b = prompt("На сколько бы вы его оценили?", "");
-        if(a === null){
-            i = i - 1;
-            continue;
-        } else if(b === null){
-            i = i - 1;
-            continue;
-        } else if(a === ""){
-            i = i - 1;
-            continue;
-        } else if(b === ""){
-            i = i - 1;
-            continue;
-        } else if(a.length >= 50){
-            i = i - 1;
-            continue;
-        } else if(b.length >= 50){
-            i = i - 1;
-            continue;
-        }
+    if( a != null && b != null && a != "" && b != "" && a.length <= 50 && b.length <= 50){
+        personalMovieDB.movies[a] = b;
+    } else {
+        i--;
+        continue;
+    }
+
+
+    //     if(a === null){
+    //         i = i - 1;
+    //         continue;
+    //     } else if(b === null){
+    //         i = i - 1;
+    //         continue;
+    //     } else if(a === ""){
+    //         i = i - 1;
+    //         continue;
+    //     } else if(b === ""){
+    //         i = i - 1;
+    //         continue;
+    //     } else if(a.length >= 50){
+    //         i = i - 1;
+    //         continue;
+    //     } else if(b.length >= 50){
+    //         i = i - 1;
+    //         continue;
+    //     }
             
-    personalMovieDB.movies[a] = b;
+    // personalMovieDB.movies[a] = b;
 }
 // personalMovieDB.movies[movie1] = ocen1;
 // personalMovieDB.movies[movie2] = ocen2;
 
 console.log(personalMovieDB);
-personalMovieDB.count < 10 ? console.log("Просмотрено довольно мало фильмов") : personalMovieDB.count > 10 && personalMovieDB.count < 30 ?
+personalMovieDB.count < 10 ? console.log("Просмотрено довольно мало фильмов") :
+personalMovieDB.count >= 10 && personalMovieDB.count <= 30 ?
 console.log("Вы классический зритель") : personalMovieDB.count > 30 ? 
 console.log("Вы киноман") : console.log("Error");
 
