dff10739b5e3f5c3eb2d4dd55f2eb9932f7d32e3
Dmitriy <letos.d.r@mail.ru>
2020-08-19 00:11:02 +0700

    add functions

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-const numberOfFilms = prompt(`Сколько фильмов вы уже посмотрели?`, ``);
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = prompt(`Сколько фильмов вы уже посмотрели?`, ``);
+
+    while(numberOfFilms  == "" || numberOfFilms == null || isNaN(numberOfFilms)){
+        numberOfFilms = prompt(`Сколько фильмов вы уже посмотрели?`, ``);
+    }
+}
+
+start();
+
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -7,20 +18,28 @@ const personalMovieDB = {
     privat: false
 };
 
-for(let i = 1; i <= 2; i++) {
-    let a = prompt("один из последних просмотренных фильмов?", "");
-    let b = prompt("На сколько бы вы его оценили?", "");
-    if( a != null && b != null && a != "" && b != "" && a.length <= 50 && b.length <= 50){
-        personalMovieDB.movies[a] = b;
-    } else {
-        i--;
-        continue;
+function rememberMyFilms(){
+    for(let i = 1; i <= 2; i++) {
+        let a = prompt("один из последних просмотренных фильмов?", "");
+        let b = prompt("На сколько бы вы его оценили?", "");
+        if( a != null && b != null && a != "" && b != "" && a.length <= 50 && b.length <= 50){
+            personalMovieDB.movies[a] = b;
+        } else {
+            i--;
+            continue;
+        }
     }
 }
+rememberMyFilms();
+
+function detectPersonalLevel(){
+    personalMovieDB.count < 10 ? console.log("Просмотрено довольно мало фильмов") :
+    personalMovieDB.count >= 10 && personalMovieDB.count <= 30 ?
+    console.log("Вы классический зритель") : personalMovieDB.count > 30 ? 
+    console.log("Вы киноман") : console.log("Error");
+   
+}
+detectPersonalLevel();
 
 console.log(personalMovieDB);
-personalMovieDB.count < 10 ? console.log("Просмотрено довольно мало фильмов") :
-personalMovieDB.count >= 10 && personalMovieDB.count <= 30 ?
-console.log("Вы классический зритель") : personalMovieDB.count > 30 ? 
-console.log("Вы киноман") : console.log("Error");
 
