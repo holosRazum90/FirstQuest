42f56fe088ad7ca0c679ed7fb0e8ff646d1e6898
Dmitriy <letos.d.r@mail.ru>
2020-08-18 23:53:02 +0700

    no comments

--- script.js
+++ script.js
@@ -6,10 +6,7 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-// const movie1 = prompt("один из последних просмотренных фильмов?", "");
-// const ocen1 = prompt("На сколько бы вы его оценили?", "");
-// const movie2 = prompt("один из последних просмотренных фильмов?", "");
-// const ocen2 = prompt("На сколько бы вы его оценили?", "");
+
 for(let i = 1; i <= 2; i++) {
     let a = prompt("один из последних просмотренных фильмов?", "");
     let b = prompt("На сколько бы вы его оценили?", "");
@@ -19,32 +16,7 @@ for(let i = 1; i <= 2; i++) {
         i--;
         continue;
     }
-
-
-    //     if(a === null){
-    //         i = i - 1;
-    //         continue;
-    //     } else if(b === null){
-    //         i = i - 1;
-    //         continue;
-    //     } else if(a === ""){
-    //         i = i - 1;
-    //         continue;
-    //     } else if(b === ""){
-    //         i = i - 1;
-    //         continue;
-    //     } else if(a.length >= 50){
-    //         i = i - 1;
-    //         continue;
-    //     } else if(b.length >= 50){
-    //         i = i - 1;
-    //         continue;
-    //     }
-            
-    // personalMovieDB.movies[a] = b;
 }
-// personalMovieDB.movies[movie1] = ocen1;
-// personalMovieDB.movies[movie2] = ocen2;
 
 console.log(personalMovieDB);
 personalMovieDB.count < 10 ? console.log("Просмотрено довольно мало фильмов") :
