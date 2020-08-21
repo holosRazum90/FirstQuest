4dcfe91a236038b24a999eb86462ef525fd06be2
Dmitriy <letos.d.r@mail.ru>
2020-08-22 00:49:37 +0700

    update func wyg

--- script.js
+++ script.js
@@ -50,9 +50,7 @@ showMyDB();
 
 function writeYourGenres() {
     for(let i = 0; i < 3; i++) {
-        let a = prompt(`Ваш любимый жанр под номером ${i + 1}?`);
-        let b = 0;
-        personalMovieDB.genres[i] = a;
+        personalMovieDB.genres[i] = prompt(`Ваш любимый жанр под номером ${i + 1}?`);
     }
 }
 writeYourGenres();
